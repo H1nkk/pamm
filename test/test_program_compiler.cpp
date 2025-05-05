6d951cefa105af6418c1f6ea218ddf8f68173e20
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-05-05 21:28:40 +0300

    Create test_program_compiler.cpp

---
+++ test/test_program_compiler.cpp
@@ -0,0 +1,98 @@
+#include <gtest/gtest.h>
+#include "expression_compiler/program_compiler.h"
+
+TEST(ProgramCompilerTest, cant_compile_without_program_name) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_without_begin_end) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_without_variables) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_with_one_variable) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_with_multiple_variables) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_if_variables_have_same_name) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_without_consts) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_with_multiple_consts) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_if_const_have_same_name) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_if_have_vars_and_consts_with_same_name) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_then_using_unknown_var_or_const) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_write) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, write_supports_many_parameters) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_write_with_no_parameters) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_read) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_read_with_no_params) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_read_with_more_than_one_parameter) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_assign) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_assign_without_right_part) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_if) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_compile_if_else) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_if_without_condition) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, cant_compile_if_with_empty_block) {
+    ADD_FAILURE();
+}
+
+TEST(ProgramCompilerTest, can_either_use_begin_end_blocks_and_one_line_operators_in_if) {
+    ADD_FAILURE();
+}
