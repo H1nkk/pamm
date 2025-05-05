8620e06039cdf610c5b5374ff2c06e84627b6bab
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-05-05 20:40:07 +0300

    postfix compiler tests

---
+++ test/test_postfix_compiler.cpp
@@ -0,0 +1,134 @@
+#include <gtest/gtest.h>
+#include "lexer/lexer.h"
+
+TEST(PostfixCompilerTest, can_compile_single_number)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_single_variable)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_sum)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_unary_minus)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixText, can_compile_multiplication)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixText, can_compile_subtraction)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixText, can_compile_float_division)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_integer_division)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_less) 
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_greater)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_equal)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_not_equal)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_lessequal)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_greater_equal) {
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_and) {
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_or)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_not) {
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_handle_operator_precedence)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_perform_type_checking)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_compile_auto_typecasting) 
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, can_perform_complex_calculation)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_unclosed_parenthesis)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_too_many_parentheses)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_empty_parentheses)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_two_operators_in_a_row)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_binary_operator_without_left_argument)
+{
+    ADD_FAILURE();
+}
+
+TEST(PostfixCompilerTest, exception_on_binary_operator_without_right_argument)
+{
+    ADD_FAILURE();
+}
