feb37e13c9844803ea8d31cf2c86b9453c3b206f
H1nkk <the_h1nk@mail.ru>
2025-05-18 16:10:04 +0300

    start work on editor

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,6 +2,10 @@
 #include <qapplication>
 #include <qpushbutton>
 #include <QElapsedTimer>
+#include <QKeyEvent>
+#include <QLineEdit>
+#include <QFont>
+#include <QItemDelegate>
 #include <string>
 #include <algorithm>
 #include <sstream>
@@ -9,7 +13,317 @@
 #include "ui_main_window.h"
 #include "ui_info_widget.h"
 #include "table.h"
-#include "calculator.h"
+
+using namespace std;
+
+class TableEditor;
+
+int globalRowWidth = 30;
+int globalCharacterWidth = 7;
+TableEditor* pInputTable;
+
+class TableEditor : public QTableWidget 
+{
+    friend class CustomDelegate;
+
+public:
+    TableEditor(QTableWidget* parent) : QTableWidget(parent) {
+        globalCharacterWidth = QFontMetrics(font()).averageCharWidth();
+    }
+    int cursorColumn = 0;
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override 
+    {
+        qDebug() << "smt is pressed";
+        if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter) 
+        {
+            int newRow = currentRow() + 1;
+
+            insertRow(newRow);
+
+            setCurrentCell(newRow, 0);
+
+            QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth , newRow * globalRowWidth });
+
+            edit(index);
+
+            return;
+        }
+        else if (event->key() == Qt::Key_Backspace) { // TODO Добавить key_delete
+            
+            qDebug() << "text: ";
+            int curRow = currentRow();
+            QTableWidgetItem* curItem = currentItem();
+
+            qDebug() << "text: " << curItem->text();
+
+            if (curItem->text() == "") {
+                removeRow(currentRow());
+            }
+            else {
+                QTableWidget::keyPressEvent(event);
+            }
+
+            return;
+        }
+        else if (event->key() == Qt::Key_Escape) { // TODO пока не работает, доделать. хотя мб это вообще не надо. надо подумать
+            editItem(item(currentRow(), 0));
+
+            return;
+        }
+        else if (event->key() == Qt::Key_Up)
+        {
+            int newRow = max(0, currentRow() - 1);
+
+            if (currentRow() == newRow) { // when newrow == 0 and currow == 0
+                cursorColumn = 0;
+            }
+
+            setCurrentCell(newRow, 0);
+
+            QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, newRow * globalRowWidth });
+
+            edit(index);
+
+            QWidget* editor = indexWidget(index);
+            if (!editor) return;
+
+            QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, newRow * globalRowWidth } - visualRect(index).topLeft();
+
+            if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+            {
+                lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+            }
+            else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+            {
+                QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                textEdit->setTextCursor(cursor);
+            }
+
+            return;
+        }
+        else if (event->key() == Qt::Key_Down) 
+        {
+            int newRow = min(rowCount() - 1, currentRow() + 1);
+
+            if (currentRow() == newRow) { // when newrow == rowCount() - 1 and currow == rowCount() - 1
+
+                cursorColumn = currentItem()->text().length(); // TODO Изменить на конец строки, а не 1000
+            }
+
+            setCurrentCell(newRow, 0);
+
+            QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, newRow * globalRowWidth });
+
+            edit(index);
+
+            QWidget* editor = indexWidget(index);
+            if (!editor) return;
+
+            QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, newRow * globalRowWidth } - visualRect(index).topLeft();
+
+            if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+            {
+                lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+            }
+            else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+            {
+                QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                textEdit->setTextCursor(cursor);
+            }
+            return;
+        }
+        else if (event->key() == Qt::Key_Left)
+        {
+            qDebug() << "left pressed";
+
+            if (cursorColumn > 0) {
+                cursorColumn = min(cursorColumn, (int)(currentItem()->text().length()));
+                cursorColumn--;
+
+                setCurrentCell(currentRow(), 0);
+
+                QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, currentRow() * globalRowWidth });
+
+                edit(index);
+
+                QWidget* editor = indexWidget(index);
+                if (!editor) return;
+
+                QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, currentRow() * globalRowWidth } - visualRect(index).topLeft();
+
+                if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+                {
+                    lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+                }
+                else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+                {
+                    QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                    textEdit->setTextCursor(cursor);
+                }
+                return;
+            }
+            else
+            {
+                if (currentRow() == 0) return;
+
+                int newRow = max(0, currentRow() - 1);
+
+                setCurrentCell(newRow, 0);
+                QTableWidgetItem* pItem = currentItem();
+                cursorColumn = pItem->text().length();
+
+                QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, newRow * globalRowWidth });
+
+                edit(index);
+
+                QWidget* editor = indexWidget(index);
+                if (!editor) return;
+
+                QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, newRow * globalRowWidth } - visualRect(index).topLeft();
+
+                if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+                {
+                    lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+                }
+                else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+                {
+                    QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                    textEdit->setTextCursor(cursor);
+                }
+
+                return;
+            }
+        }
+        else if (event->key() == Qt::Key_Right)
+        {
+            qDebug() << "right pressed";
+
+            QTableWidgetItem* pItem = currentItem();
+            int len = pItem->text().length();
+            qDebug() << len;
+            if (cursorColumn < len) {
+                cursorColumn++;
+
+                setCurrentCell(currentRow(), 0);
+
+                QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, currentRow() * globalRowWidth });
+
+                edit(index);
+
+                QWidget* editor = indexWidget(index);
+                if (!editor) return;
+
+                QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, currentRow() * globalRowWidth } - visualRect(index).topLeft();
+
+                if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+                {
+                    lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+                }
+                else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+                {
+                    QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                    textEdit->setTextCursor(cursor);
+                }
+                return;
+            }
+            else {
+                if (currentRow() == rowCount() - 1) return;
+
+                cursorColumn = 0;
+                int newRow = currentRow() + 1;
+                setCurrentCell(newRow, 0);
+
+                QModelIndex index = indexAt({ cursorColumn * globalCharacterWidth, newRow * globalRowWidth });
+
+                edit(index);
+
+                QWidget* editor = indexWidget(index);
+                if (!editor) return;
+
+                QPoint posInEditor = QPoint{ cursorColumn * globalCharacterWidth, newRow * globalRowWidth } - visualRect(index).topLeft();
+
+                if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor))
+                {
+                    lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+                }
+                else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+                {
+                    QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+                    textEdit->setTextCursor(cursor);
+                }
+                return;
+
+            }
+        }
+        else 
+        {
+            QTableWidget::keyPressEvent(event);
+        }
+    }
+
+    void mousePressEvent(QMouseEvent* event) override 
+    {
+        QTableWidget::mousePressEvent(event);
+
+        QModelIndex index = indexAt(event->pos());
+        if (!index.isValid()) return;
+
+        edit(index);
+
+        QWidget* editor = indexWidget(index);
+        if (!editor) return;
+
+        QPoint posInEditor = event->pos() - visualRect(index).topLeft();
+
+        if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor)) 
+        {
+            lineEdit->setCursorPosition(lineEdit->cursorPositionAt(posInEditor));
+            cursorColumn = lineEdit->cursorPosition();
+            qDebug() << "lineEdit index: " << cursorColumn;
+
+        }
+        else if (QTextEdit* textEdit = qobject_cast<QTextEdit*>(editor))
+        {
+            QTextCursor cursor = textEdit->cursorForPosition(posInEditor);
+            textEdit->setTextCursor(cursor);
+            cursorColumn = textEdit->textCursor().position();
+            qDebug() << "textEdit index: " << cursorColumn;
+
+        }
+
+        qDebug() << "index: " << event->pos().x() - visualRect(index).topLeft().x() << " " << event->pos().y();
+
+    }
+};
+
+class CustomDelegate : public QItemDelegate {
+public:
+    using QItemDelegate::QItemDelegate;
+
+    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+        QWidget* editor = QItemDelegate::createEditor(parent, option, index);
+
+        if (QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor)) {
+            lineEdit->installEventFilter(const_cast<CustomDelegate*>(this));
+        }
+        return editor;
+    }
+
+    bool eventFilter(QObject* editor, QEvent* event) override {
+        if (event->type() == QEvent::KeyPress) {
+            QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
+
+            if (keyEvent->key() == Qt::Key_Left || keyEvent->key() == Qt::Key_Right) {
+                qDebug() << "Left or right key pressed in editor!";
+                pInputTable->keyPressEvent(keyEvent);
+                return true;
+            }
+        }
+        return QItemDelegate::eventFilter(editor, event);
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -18,8 +332,19 @@ int main(int argc, char* argv[])
     QMainWindow window;
 
     Ui::MainWindow ui;
-
     ui.setupUi(&window);
+
+    pInputTable = new TableEditor(ui.tableWidget);
+    ui.verticalLayout->replaceWidget(ui.tableWidget, pInputTable);
+    delete ui.tableWidget;
+    ui.tableWidget = pInputTable;
+    ui.tableWidget->setItemDelegate(new CustomDelegate(ui.tableWidget));
+
+    pInputTable->setColumnCount(1);
+    pInputTable->setRowCount(1);
+    pInputTable->setColumnWidth(0, 10000);
+    pInputTable->setCursor(Qt::IBeamCursor);
+    pInputTable->setFont(QFont("Cascadia Mono")); // TODO с этой строкой иногда лагает :) Хотя лаги не из-за нее, а изза "QAbstractItemView::commitData called with an editor that does not belong to this view"
 
     window.show();
     return app.exec();
--- pamm_sln/app/ui_main_window.h
+++ pamm_sln/app/ui_main_window.h
@@ -10,10 +10,12 @@
 #define UI_MAIN_WINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpacerItem>
@@ -27,6 +29,14 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *actionasdf;
+    QAction *actionasdf_2;
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actiondfgh;
+    QAction *actiondfgh_2;
+    QAction *actiondfgh_3;
     QWidget *centralwidget;
     QVBoxLayout *verticalLayout_2;
     QVBoxLayout *verticalLayout;
@@ -36,8 +46,9 @@ public:
     QSpacerItem *horizontalSpacer;
     QVBoxLayout *verticalLayout_3;
     QTableWidget *tableWidget;
-    QSpacerItem *verticalSpacer;
     QMenuBar *menubar;
+    QMenu *menudsg;
+    QMenu *menuasdf;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -51,6 +62,22 @@ public:
         sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
         MainWindow->setSizePolicy(sizePolicy);
         MainWindow->setAutoFillBackground(false);
+        actionasdf = new QAction(MainWindow);
+        actionasdf->setObjectName("actionasdf");
+        actionasdf_2 = new QAction(MainWindow);
+        actionasdf_2->setObjectName("actionasdf_2");
+        actionNew = new QAction(MainWindow);
+        actionNew->setObjectName("actionNew");
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName("actionOpen");
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName("actionSave");
+        actiondfgh = new QAction(MainWindow);
+        actiondfgh->setObjectName("actiondfgh");
+        actiondfgh_2 = new QAction(MainWindow);
+        actiondfgh_2->setObjectName("actiondfgh_2");
+        actiondfgh_3 = new QAction(MainWindow);
+        actiondfgh_3->setObjectName("actiondfgh_3");
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         verticalLayout_2 = new QVBoxLayout(centralwidget);
@@ -80,13 +107,32 @@ public:
 
         verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3->setObjectName("verticalLayout_3");
-        verticalLayout_3->setContentsMargins(-1, -1, -1, 345);
+        verticalLayout_3->setContentsMargins(-1, -1, -1, 0);
         tableWidget = new QTableWidget(centralwidget);
         if (tableWidget->columnCount() < 1)
             tableWidget->setColumnCount(1);
         if (tableWidget->rowCount() < 2)
             tableWidget->setRowCount(2);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setItem(0, 0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setItem(1, 0, __qtablewidgetitem1);
         tableWidget->setObjectName("tableWidget");
+        tableWidget->setEnabled(true);
+        QSizePolicy sizePolicy1(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Expanding);
+        sizePolicy1.setHorizontalStretch(1);
+        sizePolicy1.setVerticalStretch(1);
+        sizePolicy1.setHeightForWidth(tableWidget->sizePolicy().hasHeightForWidth());
+        tableWidget->setSizePolicy(sizePolicy1);
+        tableWidget->setMinimumSize(QSize(0, 0));
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Cascadia Mono")});
+        font.setBold(false);
+        tableWidget->setFont(font);
+        tableWidget->viewport()->setProperty("cursor", QVariant(QCursor(Qt::CursorShape::IBeamCursor)));
+        tableWidget->setMouseTracking(true);
+        tableWidget->setFocusPolicy(Qt::FocusPolicy::ClickFocus);
+        tableWidget->setEditTriggers(QAbstractItemView::EditTrigger::AllEditTriggers);
         tableWidget->setRowCount(2);
         tableWidget->setColumnCount(1);
 
@@ -95,10 +141,6 @@ public:
 
         verticalLayout->addLayout(verticalLayout_3);
 
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
-
-        verticalLayout->addItem(verticalSpacer);
-
 
         verticalLayout_2->addLayout(verticalLayout);
 
@@ -106,10 +148,21 @@ public:
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
         menubar->setGeometry(QRect(0, 0, 789, 21));
+        menudsg = new QMenu(menubar);
+        menudsg->setObjectName("menudsg");
+        menuasdf = new QMenu(menubar);
+        menuasdf->setObjectName("menuasdf");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menudsg->menuAction());
+        menubar->addAction(menuasdf->menuAction());
+        menudsg->addAction(actionNew);
+        menudsg->addAction(actionOpen);
+        menudsg->addAction(actionSave);
+        menuasdf->addSeparator();
 
         retranslateUi(MainWindow);
 
@@ -119,8 +172,27 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "AlPo", nullptr));
+        actionasdf->setText(QCoreApplication::translate("MainWindow", "asdf", nullptr));
+        actionasdf_2->setText(QCoreApplication::translate("MainWindow", "asdf", nullptr));
+        actionNew->setText(QCoreApplication::translate("MainWindow", "New", nullptr));
+        actionOpen->setText(QCoreApplication::translate("MainWindow", "Open", nullptr));
+        actionSave->setText(QCoreApplication::translate("MainWindow", "Save", nullptr));
+        actiondfgh->setText(QCoreApplication::translate("MainWindow", "dfgh", nullptr));
+        actiondfgh_2->setText(QCoreApplication::translate("MainWindow", "dfgh", nullptr));
+        actiondfgh_3->setText(QCoreApplication::translate("MainWindow", "dfgh", nullptr));
         pushButton_2->setText(QCoreApplication::translate("MainWindow", "File", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "Run program", nullptr));
+
+        const bool __sortingEnabled = tableWidget->isSortingEnabled();
+        tableWidget->setSortingEnabled(false);
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->item(0, 0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("MainWindow", "sdfg", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->item(1, 0);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("MainWindow", "hdfg", nullptr));
+        tableWidget->setSortingEnabled(__sortingEnabled);
+
+        menudsg->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+        menuasdf->setTitle(QCoreApplication::translate("MainWindow", "Run", nullptr));
     } // retranslateUi
 
 };
