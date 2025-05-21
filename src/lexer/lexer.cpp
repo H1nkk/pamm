5f4a164b06e587bf39844a41dafaf8c08c53cc9a
Леонид Гонозов <leogonz2005@mail.ru>
2025-05-21 14:49:28 +0300

    lexer and lexer_token is done

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -5,9 +5,149 @@
 
 namespace Lexer {
 
+    static const std::unordered_map<char, TokenType> specialSymbols = {
+        { '+', TokenType::PLUS },
+        { '-', TokenType::MINUS },
+        { '*', TokenType::MULT },
+        { '.', TokenType::DOT },
+        { ',', TokenType::COMMA },
+        { ';', TokenType::SEMICOLON },
+        { ':', TokenType::COLON },
+        { '(', TokenType::LPAREN },
+        { ')', TokenType::RPAREN },
+        { '/', TokenType::DIV },
+        { '=', TokenType::EQUAL },
+        { '<', TokenType::LESS },
+        { '>', TokenType::GREATER }
+    };
+
+    static const std::unordered_map<std::string, TokenType> compositeSpecialSymbols = {
+        { ":=", TokenType::ASSIGN },
+        { "<=", TokenType::LESSEQUAL },
+        { ">=", TokenType::GREATEREQUAL },
+        { "<>", TokenType::NOTEQUAL },
+    };
+
+    static const std::unordered_map<std::string, TokenType> keywords = {
+        { "program", TokenType::PROGRAM },
+        { "const", TokenType::CONST },
+        { "var", TokenType::VAR },
+        { "begin", TokenType::BEGIN },
+        { "end", TokenType::END },
+        { "if", TokenType::IF },
+        { "then", TokenType::THEN },
+        { "else", TokenType::ELSE },
+        { "div", TokenType::DIVINT },
+        { "mod", TokenType::MOD },
+        { "and", TokenType::AND },
+        { "or", TokenType::OR },
+        { "Not", TokenType::NOT },
+    };
+
     void Lexer::generateTokens()
     {
-        
+        mTokens.clear();
+
+        size_t index = 0;
+
+        while (mTokens.size() == 0 ||
+            (mTokens.back().type() != TokenType::ENDOFFILE &&
+                mTokens.back().type() != TokenType::INVALID))
+        {
+            while (index < mText.size() && isspace(mText[index]))
+                ++index;
+            if (index >= mText.size())
+            {
+                // Выражение закончилось
+                mTokens.push_back(Token(TokenType::ENDOFFILE, "", index, index));
+            }
+            else if (specialSymbols.find(mText[index]) != specialSymbols.end()) // найденный элемент находится в map
+            {
+                if (index + 1 < mText.size()) {
+                    if (compositeSpecialSymbols.find(mText.substr(index, 2)) != compositeSpecialSymbols.end())
+                    {
+                        TokenType type = compositeSpecialSymbols.at(mText.substr(index, 2));
+                        Token tok = Token(type, std::string(2, mText[index]), index, index + 2);
+                        mTokens.push_back(tok);
+                        index += 2;
+                    }
+                }
+                else
+                {
+                    TokenType type = specialSymbols.at(mText[index]);
+                    Token tok = Token(type, std::string(1, mText[index]), index, index + 1);
+                    mTokens.push_back(tok);
+                    ++index;
+                }
+            }
+            else if (isdigit(mText[index]))
+            {
+                // Найдено число
+                std::string val;
+                size_t delimeterCount = 0;
+
+                while (index < mText.size() &&
+                    (mText[index] == '.' || isdigit(mText[index])))
+                {
+                    val.push_back(mText[index++]);
+
+                    if (val.back() == '.')
+                    {
+                        ++delimeterCount;
+                        if (delimeterCount >= 2) break;
+                    }
+                }
+
+                if (delimeterCount == 0)
+                    // Целое число
+                    mTokens.push_back(Token(TokenType::INT, val, index - val.size(), index));
+                else if (delimeterCount == 1 && val.back() != '.')
+                    // Число с плавающей точкой
+                    mTokens.push_back(Token(TokenType::FLOAT, val, index - val.size(), index));
+                else
+                    // Лишняя точка приводит к занесению всего числа в некорректные
+                    mTokens.push_back(Token(TokenType::INVALID, val,
+                        index - val.size(), index));
+            }
+            else if (mText[index] == '\'')
+            {
+                // Найдена строка 
+                std::string str;
+                while (index < mText.size() && mText[index] != '\'')
+                    str.push_back(mText[index++]);
+
+                if (str.back() == '\'')
+                    mTokens.push_back(Token(TokenType::STRING, str, index - str.size(), index));
+                else
+                    mTokens.push_back(Token(TokenType::INVALID, str, index - str.size(), index));
+            }
+            else if (mText[index] == '_' || isalpha(mText[index]))
+            {
+                std::string id;
+                while (index < mText.size() &&
+                    (mText[index] == '_' || isalnum(mText[index])))
+                    id.push_back(mText[index++]);
+
+                if (keywords.find(id) != keywords.end())
+                {
+                    TokenType type = keywords.at(id);
+                    Token tok = Token(type, id, index - id.size(), index);
+                    mTokens.push_back(tok);
+                }
+                else
+                    mTokens.push_back(Token(TokenType::ID, id, index - id.size(), index));
+
+            }
+            else
+                mTokens.push_back(Token(TokenType::INVALID, std::string(1, mText[index]), index, index + 1));
+
+
+
+        }
     }
+
+
 }
+
+
 
--- src/lexer/lexer_token.cpp
+++ src/lexer/lexer_token.cpp
@@ -4,8 +4,49 @@ namespace Lexer {
 
     std::string toString(TokenType type)
     {
-        return "";
+        switch (type)
+        {
+        case TokenType::NONE: return "NONE";
+        case TokenType::FLOAT: return "FLOAT";
+        case TokenType::INT: return "INT";
+        case TokenType::ID: return "ID";
+        case TokenType::STRING: return "STRING";
+        case TokenType::PROGRAM: return "PROGRAM";
+        case TokenType::CONST: return "CONST";
+        case TokenType::VAR: return "VAR";
+        case TokenType::BEGIN: return "BEGIN";
+        case TokenType::END: return "END";
+        case TokenType::DOT: return "DOT";
+        case TokenType::COMMA: return "COMMA";
+        case TokenType::SEMICOLON: return "SEMICOLON";
+        case TokenType::COLON: return "COLON";
+        case TokenType::LPAREN: return "LPAREN";
+        case TokenType::RPAREN: return "RPAREN";
+        case TokenType::IF: return "IF";
+        case TokenType::THEN: return "THEN";
+        case TokenType::ELSE: return "ELSE";
+        case TokenType::ASSIGN: return "ASSIGN";
+        case TokenType::PLUS: return "PLUS";
+        case TokenType::MINUS: return "MINUS";
+        case TokenType::MULT: return "MULT";
+        case TokenType::DIV: return "DIV";
+        case TokenType::DIVINT: return "DIVINT";
+        case TokenType::MOD: return "MOD";
+        case TokenType::EQUAL: return "EQUAL";
+        case TokenType::LESSEQUAL: return "LESSEQUAL";
+        case TokenType::LESS: return "LESS";
+        case TokenType::GREATER: return "GREATER";
+        case TokenType::GREATEREQUAL: return "GREATEREQUAL";
+        case TokenType::NOTEQUAL: return "NOTEQUAL";
+        case TokenType::AND: return "AND";
+        case TokenType::OR: return "OR";
+        case TokenType::NOT: return "NOT";
+        case TokenType::INVALID: return "INVALID";
+        case TokenType::ENDOFFILE: return "ENDOFFILE";
+        }
+        return "UNKNOWN"; // Fallback for any unhandled cases
     }
+
 
     bool operator==(const Token& l, const Token& r)
     {
